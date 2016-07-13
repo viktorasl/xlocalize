f73c5bf565cfd516cd03e13a81dda796acc72d69
Viktoras Laukevicius <viktoras.laukevicius@yahoo.com>
2016-07-13 17:05:51 +0300

    Export method accepting provided parameters

--- lib/xlocalise.rb
+++ lib/xlocalise.rb
@@ -7,55 +7,29 @@ module Xlocalise
   class XlocaliseCLI
     include Commander::Methods
 
-    def global_opts_valid(global_opts)
-      if global_opts[:project].nil? or
-         global_opts[:target].nil? or
-         global_opts[:excl_prefix].nil? or
-         global_opts[:master_lang].nil?
-        return false
-      else
-        return true
-      end
-    end
-
     def run
       program :name, 'Xlocalise'
       program :version, Xlocalise::VERSION
       program :description, Xlocalise::DESCRIPTION
 
-      global_opts = {}
-      wti = nil
-
-      global_option('--wti_key KEY', '-w', 'Webtranslateit API key'){ |key| wti = WebtranslateIt.new(key) }
-      global_option('--project PROJECT', '-p', 'Path to project file'){ |project| global_opts[:project] = project }
-      global_option('--target TARGET', '-t', 'Target in the project'){ |target| global_opts[:target] = target }
-      global_option('--excl_prefix EXCL_PREFIX', '-e', 'Exclude strings having specified prefix'){ |excl_prefix| global_opts[:excl_prefix] = excl_prefix }
-      global_option('--master_lang MASTER_LANG', '-m', 'Master language of the project'){ |master_lang| global_opts[:master_lang] = master_lang }
-
       command :export do |c|
         c.syntax = 'xlocalise export [options]'
         c.description = 'Export localised strings from Xcode project'
+        c.option '--wti_key STRING', String, 'Webtranslateit API key'
+        c.option '--project STRING', String, 'Path to project file'
+        c.option '--target STRING', String, 'Target in the project'
+        c.option '--excl_prefix STRING', String, 'Exclude strings having specified prefix'
+        c.option '--master_lang STRING', String, 'Master language of the project'
         c.action do |args, options|
-          if global_opts_valid(global_opts)
-            xlc = Executor.new(wti, global_opts[:project], global_opts[:target], global_opts[:excl_prefix], global_opts[:master_lang])
-            xlc.export_master
-          else
-            puts 'Missing definitions of global options'
+          if options.project.nil? or
+             options.target.nil? or
+             options.excl_prefix.nil? or
+             options.master_lang.nil?
+            raise 'Missing parameter'
           end
-        end
-      end
 
-      command :import do |c|
-        c.syntax = 'xlocalise import [options]'
-        c.description = 'Import localised strings to Xcode project'
-        c.option '--locales ARRAY', Array, 'Imports localised strings from Webtranslateit for given locales'
-        c.action do |args, options|
-          options.default :locales => []
-          if global_opts_valid(global_opts)
-            puts 'Import'
-          else
-            puts 'Missing definitions of global options'
-          end
+          wti = WebtranslateIt.new(options.wti_key) if !options.wti_key.nil?
+          xlc = Executor.new.export_master(wti, options.project, options.target, options.excl_prefix, options.master_lang)
         end
       end
 
@@ -63,5 +37,3 @@ module Xlocalise
     end
   end
 end
-
-Xlocalise::XlocaliseCLI.new.run if __FILE__ == $0
--- lib/xlocalise/executor.rb
+++ lib/xlocalise/executor.rb
@@ -4,42 +4,31 @@ require 'nokogiri'
 
 module Xlocalise
   class Executor
-    attr_reader :project, :taget, :master_lang, :excl_prefix
-    attr_reader :wti
-
-		def initialize(wti, project, target, excl_prefix, master_lang)
-      @wti = wti
-
-			@project = project
-			@target = target
-			@excl_prefix = excl_prefix
-			@master_lang = master_lang
-		end
 
 		def locale_file_name(locale)
 			return "#{locale}.xliff"
 		end
 
-		def export_master
-			master_file_name = locale_file_name(@master_lang)
+		def export_master(wti, project, target, excl_prefix, master_lang)
+			master_file_name = locale_file_name(master_lang)
 			
-			system "xcodebuild -exportLocalizations -localizationPath ./ -project #{@project}"
-			purelyze(master_file_name)
+			system "xcodebuild -exportLocalizations -localizationPath ./ -project #{project}"
+			purelyze(master_file_name, target, excl_prefix)
 
       # Pushing master file to WebtranslateIt
       begin
         puts "Uploading master file to WebtranslateIt"
         File.open(master_file_name, "r") {|file|
-          @wti.push_master(file)
+          wti.push_master(file)
           puts "Done.".green
         }
       rescue => err
         puts err.to_s.red
-      end if !@wti.nil?
+      end if !wti.nil?
 		end
 
-		def purelyze(locale_file_name)
-			target_prefix = "#{@target}/"
+		def purelyze(locale_file_name, target, excl_prefix)
+			target_prefix = "#{target}/"
 			doc = Nokogiri::XML(open(locale_file_name))
 
 			puts "Removing all files not matching required targets"
@@ -50,7 +39,7 @@ module Xlocalise
 
 			puts "Removing trans-unit's having reserverd prefix in their sources"
 			doc.xpath("//xmlns:source").each { |node|
-				node.parent.remove if node.content.start_with?(@excl_prefix)
+				node.parent.remove if node.content.start_with?(excl_prefix)
 			}
 
 			puts "Removing all files having no trans-unit elements after removal"
@@ -60,15 +49,6 @@ module Xlocalise
 
 			puts "Writing modified XLIFF file to #{locale_file_name}"
 			File.open(locale_file_name, "w") {|file| file.write(doc.to_xml) }
-		end
-
-		def export(locales)
-			exportLanguages = locales.map {|locale| "-exportLanguage #{locale}"}.join(" ")
-			system "xcodebuild -exportLocalizations -localizationPath ./ -project #{@project} #{exportLanguages}"
-
-			locales.each {|locale|
-				purelyze(locale_file_name(locale))
-			}
 		end
   end
 end
