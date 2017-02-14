3d50b003e054258a6648da3ffc0f3a11629e3b74
Viktoras Laukevicius <viktoras.laukevicius@yahoo.com>
2017-02-14 14:23:10 +0200

    Fixed xliff & plurals upload to WebtranslateIt

--- lib/xlocalize/webtranslateit.rb
+++ lib/xlocalize/webtranslateit.rb
@@ -30,6 +30,7 @@ module Xlocalize
     end
 
     def push_master(file, plurals_file, override = true)
+      puts 'Updating xliff file'
       # uploding master xliff file
       request = Net::HTTP::Put::Multipart.new("/api/projects/#{@key}/files/#{@xliff_file_id}/locales/#{@source_locale}", {
         "file" => UploadIO.new(file, "text/plain", file.path),
@@ -45,23 +46,33 @@ module Xlocalize
       }
 
       if @plurals_file_id.nil?
+        puts 'Creating plurals file'
         # /api/projects/:project_token/files [POST]
-        request = Net::HTTP::Put::Multipart.new("/api/projects/#{@key}/files", {
-          "file" => UploadIO.new(file, "text/plain", plurals_file.path),
+        request = Net::HTTP::Post::Multipart.new("/api/projects/#{@key}/files", {
+          "file" => UploadIO.new(plurals_file, "text/plain", plurals_file.path),
           "name" => "plurals.yaml",
           "low_priority" => false
         })
-        @http.request(request)
+        @http.request(request) { |res|
+          if !res.code.to_i.between?(200, 300)
+            raise JSON.parse(res.body)["error"]
+          end
+        }
       else
+        puts 'Updating plurals file'
         # /api/projects/:project_token/files/:master_project_file_id/locales/:locale_code [PUT]
-        request = Net::HTTP::Post::Multipart.new("/api/projects/#{@key}/files/#{@plurals_file_id}/locales/#{@source_locale}", {
+        request = Net::HTTP::Put::Multipart.new("/api/projects/#{@key}/files/#{@plurals_file_id}/locales/#{@source_locale}", {
           "file" => UploadIO.new(plurals_file, "text/plain", plurals_file.path),
           "merge" => !override,
           "ignore_missing" => true,
           "label" => "",
           "low_priority" => false
         })
-        @http.request(request)
+        @http.request(request) { |res|
+          if !res.code.to_i.between?(200, 300)
+            raise JSON.parse(res.body)["error"]
+          end
+        }
       end
     end
 
